f80cc2ce2d57eeec1bbbeb7ff837386ed8c00158
massindia <mr.perfect@live.co.uk>
2016-12-05 12:09:12 +0530

    Update MountainNumber.java

--- src/MountainNumber.java
+++ src/MountainNumber.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 
-public class MountainNumber {
+
+public class heighst {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner (System.in);
+		
+		System.out.println("Enter 1st Number");
+		int a = scan.nextInt();
+		System.out.println("Enter 2nd Number");
+		int b = scan.nextInt();
+		System.out.println("Enter 3rd Number");
+		int c = scan.nextInt();
+
+		if(a>b && a>c)//comparing a with b and c
+			System.out.println("1st Number is largest"+ a );
+		else if (b>a && b>c)//comparing b with a and c
+			System.out.println("2nd Number is largest"+ b);
+		else
+			System.out.println("3rd Number is largest"+ c);
+	}
 
 }
